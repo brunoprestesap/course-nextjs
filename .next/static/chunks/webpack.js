ef45759d56561a22b56f30635e6b95b7d7b33c26
Bruno Prestes <brunoasprestes@gmail.com>
2023-10-03 15:46:21 -0300

    adicionando page faq

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b59d67818a6f5084"; }
+/******/ 		__webpack_require__.h = function() { return "d895ccb6baf408c2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
